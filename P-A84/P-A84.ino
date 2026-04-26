6eaa835e3c545d02f6c8649ee85c06461e2e278b
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2026-04-26 17:10:51 +0200

    unsuccesfull debugging

--- P-A84/P-A84.ino
+++ P-A84/P-A84.ino
@@ -1,11 +1,40 @@
 #include <Arduino.h>
 #include "pins.h"
 
+void setupPins()
+{
+    pinMode(rowDataPin, OUTPUT);
+    pinMode(rowClockPin, OUTPUT);
+    pinMode(rowLatchPin, OUTPUT);
 
+    pinMode(colDataPin, OUTPUT);
+    pinMode(colClockPin, OUTPUT);
+    pinMode(colLatchPin, OUTPUT);
 
-void setup() {
+    pinMode(btnLeftPin, INPUT_PULLUP);
+    pinMode(btnOkPin, INPUT_PULLUP);
+    pinMode(btnCancelPin, INPUT_PULLUP);
+    pinMode(btnRightPin, INPUT_PULLUP);
+}
+
+void setup() 
+{
+    setupPins();
 
 }
 
-void loop() {
+void loop() 
+{
+    delay(100);
+    digitalWrite(colLatchPin, LOW);
+    digitalWrite(rowLatchPin, LOW);
+    delay(100);
+    shiftOut(colDataPin, colClockPin, MSBFIRST, 0x01111110);
+    delay(100);
+    shiftOut(rowDataPin, rowClockPin, MSBFIRST, 0x01111110);
+
+    delay(100);
+    digitalWrite(colLatchPin, HIGH);
+    digitalWrite(rowLatchPin, HIGH);
+    delay(1000);
 }
