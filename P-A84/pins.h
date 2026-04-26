073f315ddbb1ad28c60612a1dfa7fe39a8c761b9
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2026-04-26 16:11:06 +0200

    pins config

---
+++ P-A84/pins.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+extern const int rowDataPin;
+extern const int rowClockPin;
+extern const int rowLatchPin;
+
+extern const int colDataPin;
+extern const int colClockPin;
+extern const int colLatchPin;
+
+extern const int btnLeftPin;
+extern const int btnOkPin;
+extern const int btnCancelPin;
+extern const int btnRightPin;
+
+#endif
---
+++ P-A84/pins_config.cpp
@@ -0,0 +1,14 @@
+#include "pins.h"
+
+const int rowDataPin = 5;
+const int rowClockPin = 0;
+const int rowLatchPin = 1;
+
+const int colDataPin = 6;
+const int colClockPin = 3;
+const int colLatchPin = 2;
+
+const int btnLeftPin = 10;
+const int btnOkPin = 9;
+const int btnCancelPin = 8;
+const int btnRightPin = 7;
