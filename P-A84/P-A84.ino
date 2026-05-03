6ca5b2e7af1c4c2c72ecb127f068a8f6b8781ccd
Ichigo002 <87382616+Ichigo002@users.noreply.github.com>
2026-05-03 16:51:56 +0200

    add menu handling

--- P-A84/P-A84.ino
+++ P-A84/P-A84.ino
@@ -1,12 +1,12 @@
 #include <Arduino.h>
-#include "pins.h"
 #include "TextRenderer.h"
-#include <avr/pgmspace.h>
-#include <TimerKernel.h>
+#include "InputHandler.h"
+#include "ActionHandler.h"
 
 TextRenderer renderer;
-TimerKernel timer_change_char;
-int i = 0;
+InputHandler input;
+
+ActionHandler action_handler(&renderer, &input);
 
 void setupPins()
 {
@@ -19,28 +19,107 @@ void setupPins()
     pinMode(colLatchPin, OUTPUT);
 
     pinMode(btnLeftPin, INPUT);
-    pinMode(btnOkPin, INPUT);
-    pinMode(btnCancelPin, INPUT);
+    pinMode(btnUpPin, INPUT);
+    pinMode(btnDownPin, INPUT);
     pinMode(btnRightPin, INPUT);
 }
 
-void setup() 
+char option = 0;
+bool option_is_chosen = false;
+bool btn_pressed = true;
+
+void updateMenu()
 {
-    setupPins();
-    renderer.renderAnimatedText("XD", 2, 100, true);
-    //renderer.renderASCII(' ');
+    if (option_is_chosen)
+        return;
+
+    if (input.isButtonPressed(BTN_LEFT))
+    {
+        option--;
+        btn_pressed = true;
+        if (option < 0)
+            option = 4;
+    }
+
+    if (input.isButtonPressed(BTN_RIGHT))
+    {
+        option++;
+        btn_pressed = true;
+        if (option > 4)
+            option = 0;
+    }
+
+    if (input.isButtonPressed(BTN_UP))
+    {
+        btn_pressed = true;
+        option_is_chosen = true;
+        return;
+    }
+
+    if (!btn_pressed)
+        return;
+
+    btn_pressed = false;
+
+    switch (option)
+    {
+    case 0:
+        renderer.renderAnimatedText("P1BROWSE CHARS", 14, true);
+        break;
+    case 1:
+        renderer.renderAnimatedText("P2EXAMPLE ANIMATION", 19, true);
+        break;
+    case 2:
+        renderer.renderAnimatedText("P3CUSTOM ANIMATION", 18, true);
+        break;
+    case 3:
+        renderer.renderAnimatedText("P4TETRIS", 8, true);
+        break;
+    case 4:
+        renderer.renderAnimatedText("P4SNAKE", 7, true);
+        break;
+    default:
+        renderer.renderAnimatedText("ERROR MENU", 10, true);
+        break;
+    }
 }
 
-void loop() 
+void setup()
 {
-    // if(timer_change_char.hasExpired(1000))
-    // {
-    //     if(i>44)
-    //         i = 0;
-    //     Glyph g = _readASCII(i);
-    //     renderer.renderGlyph(g);
-    //     i++;
-    //     timer_change_char.resetHasExpired();
-    // }
+    setupPins();
+}
+
+void loop()
+{
+    updateMenu();
+
+    if (option_is_chosen)
+    {
+        switch (option)
+        {
+        case 0: // browse chars
+            action_handler.runBrowseChars();
+            break;
+        case 1: // example animation
+            action_handler.runExampleAnimation();
+            break;
+        case 2: // custom animations
+            action_handler.runCustomAnimation();
+            break;
+        case 3: // tetris
+
+            break;
+        case 4: // snake
+
+            break;
+        }
+
+        if (input.isButtonPressedFor2Sec(BTN_DOWN)) // EXIT
+        {
+            action_handler.resetStates();
+            option_is_chosen = false;
+        }
+    }
+
     renderer.update();
 }
